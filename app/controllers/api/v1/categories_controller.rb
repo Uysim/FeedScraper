e50083f770d4c5443144a28ceff4859bba51c203
Uysim <uysimty@gmail.com>
2016-03-31 21:37:33 +0700

    enable real data

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::CategoriesController < ApplicationController
   def show
-    # @category = Category.find(params[:id])
-    if params[:page].to_i==0
-      category = JSON.parse(File.read('public/json_file/categories/1.json'))
-      render json: category
-    else
-      category = {
-        name: "Apple",
-        website: {
-          id: 1,
-          name: "CNET",
-        },
-        contents: []
-      }
-      render json: category
-    end
+    @category = Category.find(params[:id])
+    # if params[:page].to_i==0
+    #   category = JSON.parse(File.read('public/json_file/categories/1.json'))
+    #   render json: category
+    # else
+    #   category = {
+    #     name: "Apple",
+    #     website: {
+    #       id: 1,
+    #       name: "CNET",
+    #     },
+    #     contents: []
+    #   }
+    #   render json: category
+    # end
   end
 end
--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ContentsController < ApplicationController
   def show
-    # @content = Content.find(params[:id])
-    content = JSON.parse(File.read('public/json_file/contents/1.json'))
-    render json: content
+    @content = Content.find(params[:id])
+    # content = JSON.parse(File.read('public/json_file/contents/1.json'))
+    # render json: content
   end
 end
